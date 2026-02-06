97dd9686f4b35e9c55db776d74b1e0fffbfbf893
mohamedhennihm <omhenni@esi.dz>
2026-02-06 05:40:36 +0100

    third commit

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 // Function to connect to MongoDB
 export const connectDB = async () => {
   try {
-    if (!process.env.URI_DB) {
+    if (!process.env.DB_URI) {
       throw new Error("MongoDB connection string (URI_DB) is not defined in .env");
     }
 
-    await mongoose.connect(process.env.URI_DB);
+    await mongoose.connect(process.env.DB_URI);
 
     console.log("MongoDB connected successfully");
   } catch (err) {
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { register,login,logout } from "../controllers/authController";
+import { register,login,logout } from "../controllers/authController.js";
 
 const authRoute = Router()
 
